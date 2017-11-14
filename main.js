ada0340490057ce2f84a6aa93982cf6697f1d60d
zkjaisq <18771964684@163.com>
2017-11-14 13:00:06 +0800

    changed

--- main.js
+++ main.js
@@ -1,103 +1,126 @@
-var keys = {
-    0: ['1','2','3','4','5','6','7','8','9','0' ],
-    1: ['q','w','e','r','t','y','u','i','o','p'],
-    2: ['a','s','d','f','g','h','j','k','l'],
-    3: ['z','x','c','v','b','n','m',],
-   length:4,
+
+function init() {
+    var keys = {
+        0: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+        1: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+        2: ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+        3: ['z', 'x', 'c', 'v', 'b', 'n', 'm',],
+        length: 4,
+    }
+    var hash = {
+        'q': 'qq.com',
+        'w': 'weibo.com',
+        't': 'taobao.com',
+        'i': 'iqiyi.com',
+        'a': 'amazon.com',
+        'd': 'douban.com',
+        'f': 'fenghuang.com',
+        'j': 'jindong.com',
+        'z': 'zhilian.com',
+    }
+    //取出localstroge中存的web对应的网址
+    hashInLocalStorage = getFromeloclastorage('web')
+    if (hashInLocalStorage) {
+        hash = hashInLocalStorage
+    }
+    return {
+        "keys": keys,
+        "hash": hash
+    }
 }
-var  hash = {
-    '2': '163.com',
-    '3': 'qq.com',
-    '5': 'tianya.cn',
-    'q': 'qq.com',
-    'w': 'weibo.com',
-    't': 'taobao.com',
-    'i': 'iqiyi.com',
-    'a': 'acfun.cn',
-    'd': 'douban.com',
-    'j': 'jd.com',
-    'z': 'zhihu.com',
+function getFromeloclastorage(name) {
+    return JSON.parse(localStorage.getItem(name) || 'null')
 }
-function getFromeloclastorage(name){
-   return JSON.parse(localStorage.getItem(name)|| 'null')
-}
-function tag(tagname){
+function tag(tagname) {
     return document.createElement(tagname)
 }
-function createSPAN(textContent){
+function textContent(textContent) {
     var span = tag('span')
-    span.textContent = text
+    span.textContent = textContent
     span.className = "text"
-    div2.appendChild(span)
-
+    return span
 }
-for(index = 0;index< keys['length']; index=index+1){
-    var div1 = tag('div') 
-    main.appendChild(div1)
+function Button(id) {
+    var button = tag('button') //生成button标签，
 
-    var row = keys[index]
-for(index2 =0; index2 < row['length']; index2 = index2+1){
-    var div2 = tag('kbd')
-    div2.className = "key"
-    div1.appendChild(div2)
-
-    var span = tag('span')
-    span.textContent = row[index2]
-    span.className = "text"
-    div2.appendChild(span)
-
-    var buttom1 = tag('button') //生成button标签，
-    div2.appendChild(buttom1)
-   
-    buttom1.textContent = 'E'
-    buttom1.id = row[index2]
-    buttom1.onclick = function(xzkjcnxlkcjlk){
+    button.textContent = 'E'
+    button.id = id
+    button.onclick = function (xzkjcnxlkcjlk) {
         var button = xzkjcnxlkcjlk['target']
         var button3 = button.nextSibling
-        var img2 =  button3.nextSibling
+        var img2 = button3.nextSibling
         console.log(button3.nextSibling)
         var key = button['id']
         var x = prompt('给我一个网址')
         hash[key] = x
-       img2.src = 'http://'+x + './favicon.ico'
-       console.log(img2.src)
-       img2.onerror = function(xxx){
-        xxx.target.src = 'https://i.loli.net/2017/11/13/5a092f5e689ec.png'
+        img2.src = 'http://' + x + './favicon.ico'
+        console.log(img2.src)
+        img2.onerror = function (xxx) {
+            xxx.target.src = 'https://i.loli.net/2017/11/13/5a092f5e689ec.png'
+        }
+        localStorage.setItem('web', JSON.stringify(hash))
     }
-       localStorage.setItem('web', JSON.stringify(hash))
+    return button
 }
-    
-    var buttom2 = tag('span') //生成button标签，
-    div2.appendChild(buttom2)
-    div1.appendChild(div2)
-    buttom2.textContent ='D'
-    buttom2.id = "delete"
-    
-    var buttom3 = tag('img')
-    div2.appendChild(buttom3)
-    div1.appendChild(div2)
-    if(hash[row[index2]]){
-        buttom3.src = 'http://'+hash[row[index2]] + './favicon.ico'
-    }else{
-        buttom3.src = 'https://i.loli.net/2017/11/13/5a092f5e689ec.png'
+function span() {
+    var span = tag('span') //生成button标签，
+    span.textContent = 'D'
+    span.id = "delete"
+    return span
+}
+function createImg(domain) {
+    var img = tag('img')
+
+    if (domain) {
+        img.src = 'http://' + domain + './favicon.ico'
+    } else {
+        img.src = 'https://i.loli.net/2017/11/13/5a092f5e689ec.png'
     }
-    buttom3.onerror = function(xxx){
+    img.onerror = function (xxx) {
         xxx.target.src = 'https://i.loli.net/2017/11/13/5a092f5e689ec.png'
     }//监听报错时间，如果报错就还是将我们的图片给它。
     //点击屏幕的时候，输入一个网址，将网址储存在浏览器中。
-  
+    return img
+}
+function generatekeyboard(keys, hash) {
+    for (index = 0; index < keys['length']; index = index + 1) {
+        var div = tag('div')
+        main.appendChild(div)
+
+        var row = keys[index]
+        for (index2 = 0; index2 < row['length']; index2 = index2 + 1) {
+            var kbd = tag('kbd')
+            kbd.className = "key"
+
+            var span = textContent(row[index2])
+            var button = Button(row[index2])
+            var span = span
+            var img = createImg(hash[row[index2]])
+
+            div.appendChild(kbd)
+            kbd.appendChild(button)
+            kbd.appendChild(span)
+            kbd.appendChild(span)
+            kbd.appendChild(img)
+        }
+    }
+    return
+}
+function listernTOkeyboard(hash) {
+    document.onkeypress = function (xzkjcnxlkcjlk) {
+        var key = xzkjcnxlkcjlk['key']
+        var website = hash[key]
+        window.open('http://' + website, '_blank')
+    }//监听键盘事件，按键时候将所需要的网址弄出来，在新的窗口打开网址。
+    return
 }
 
-}
-//取出localstroge中存的web对应的网址
-hashInLocalStorage = getFromeloclastorage('web')
-if(hashInLocalStorage){
-    hash = hashInLocalStorage
-}
+//初始化数据
+var hashA = init()
+var keys = hashA['keys']
+var hash = hashA['hash']
+//生成键盘
+generatekeyboard(keys, hash)
+//监听事件
+listernTOkeyboard(hash)
 
-
- document.onkeypress = function(xzkjcnxlkcjlk){
-    var key = xzkjcnxlkcjlk['key']
-    var website =hash[key]
-    window.open('http://'+website, '_blank')
-}//监听键盘事件，按键时候将所需要的网址弄出来，在新的窗口打开网址。
